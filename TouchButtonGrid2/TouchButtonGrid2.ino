4d393b6897308c519bd81aa962c54e4969af5135
Dean Arvonen <deanarvonen@MacBook-Pro.local>
2019-11-29 17:46:54 +1100

    Oops wrong code, this is the one

--- TouchButtonGrid2/TouchButtonGrid2.ino
+++ TouchButtonGrid2/TouchButtonGrid2.ino
@@ -1,57 +1,47 @@
-//
-// Created by kiryanenko on 14.09.19.
-//
-
-//////////////////////////////////////////////////////////////////
-// Connections
-//
-// Uno:   SDA <-> A4 (SDA)
-//        SCL <-> A5 (SCL)
-//
-// Mega:  SDA <-> 20 (SDA)
-//        SCL <-> 21 (SCL)
-//
-// Leo:   SDA <-> 2 (SDA)
-//        SCL <-> 3 (SCL)
-//
-//////////////////////////////////////////////////////////////////
-
-
-#include <Wire.h>
-#include <TTP229.h>
-
-TTP229 ttp229;
-
+/*  ___   ___  ___  _   _  ___   ___   ____ ___  ____  
+ * / _ \ /___)/ _ \| | | |/ _ \ / _ \ / ___) _ \|    \ 
+ *| |_| |___ | |_| | |_| | |_| | |_| ( (__| |_| | | | |
+ * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
+ *                  (____/    
+ * www.osoyoo.com water drop sensor
+ * program tutorial :http://osoyoo.com/2016/07/11/capacitive-touchpad-16channel/
+ *  Copyright John Yu
+ */
+int SCLPin = A5, SDOPin = A4;
+unsigned int DATA = 0, old_DATA = 0;
 void setup()
 {
-    Wire.begin();
-    Serial.begin(9600);
+  Serial.begin(9600);
+  pinMode(SCLPin , OUTPUT);   //时钟端输出
+}
+
+unsigned int ttp229()     //一次读完TTP229所有按键
+{
+  unsigned int DATA = 0;
+  pinMode(SDOPin , OUTPUT);
+  digitalWrite(SDOPin, HIGH);
+  delayMicroseconds(93);
+  digitalWrite(SDOPin, LOW);
+  delayMicroseconds(10);
+  pinMode(SDOPin , INPUT);
+  for (int i = 0; i < 16; i++)
+  {
+    digitalWrite(SCLPin, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(SCLPin, LOW);
+    DATA |= digitalRead(SDOPin) << i;
+  }
+  delay(2);
+  return DATA&0xFFFF;
 }
 
 void loop()
 {
-    ttp229.readKeys();
-
-    Serial.print("Key states: ");
-    for (int i = 0; i < 16; ++i) {
-        if (ttp229.isKeyPress(i)) {
-            Serial.print("1 ");
-        } else {
-            Serial.print("0 ");
-        }
-    }
-
-    int key = ttp229.getKey();
-    Serial.print("Pressed key: ");
-    Serial.print(key);
-
-    Serial.print("   ");
-    if (ttp229.isKeyDown(0)) {
-        Serial.print("Key 0 is down");
-    } else if (ttp229.isKeyUp(0)) {
-        Serial.print("Key 0 is up");
-    }
-
-    Serial.println();
+  DATA = ttp229();
+  if (DATA != old_DATA)
+  {
+    Serial.println(DATA, BIN);
+    old_DATA = DATA;
     delay(100);
+  }
 }
